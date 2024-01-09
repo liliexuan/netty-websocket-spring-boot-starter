3f19e560e7b1070569b52e2c5946361c6050ada5
liliexuan <liliexuan@qibingdaojia.com>
2024-01-09 17:26:37 +0800

    ServerEndpoint增加注解Component

--- src/main/java/org/yeauty/annotation/ServerEndpoint.java
+++ src/main/java/org/yeauty/annotation/ServerEndpoint.java
@@ -1,6 +1,7 @@
 package org.yeauty.annotation;
 
 import org.springframework.core.annotation.AliasFor;
+import org.springframework.stereotype.Component;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -10,6 +11,7 @@ import java.lang.annotation.Target;
 /**
  * @author Yeauty
  */
+@Component
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface ServerEndpoint {
