920420716489953744bb47081c9288da1c4a46d7
liliexuan <liliexuan@qibingdaojia.com>
2024-01-10 09:24:23 +0800

    Revert "ServerEndpoint增加注解Component"
    This reverts commit b4dc005d1b1827e6016e1abf87ae2b229450405e.

--- src/main/java/org/yeauty/annotation/ServerEndpoint.java
+++ src/main/java/org/yeauty/annotation/ServerEndpoint.java
@@ -1,5 +1,6 @@
 package org.yeauty.annotation;
 
+import org.springframework.core.annotation.AliasFor;
 import org.springframework.stereotype.Component;
 
 import java.lang.annotation.ElementType;
@@ -15,6 +16,10 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 public @interface ServerEndpoint {
 
+    @AliasFor("path")
+    String value() default "/";
+
+    @AliasFor("value")
     String path() default "/";
 
     String host() default "0.0.0.0";
